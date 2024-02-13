b28b7ff83a2288ac7155a389c3049f53272f042b
Elina <elinasarkina17@gmail.com>
2024-02-13 17:20:46 +0100

    add task_hw6

---
+++ Task_HW6/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+char[,]charArray = new char[,]
+{
+    {'a','1','c'},
+    {'d','e','F'}
+};
+string result = " ";
+for (int i = 0; i < charArray.GetLength(0); i++)
+{
+ for (int j = 0; j < charArray.GetLength(1); j++)
+ {
+    result += charArray[i,j];
+ }   
+}
+Console.WriteLine(result);
+
+//Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string originalString = "HeLLO WorLD";
+string LowerCaseString = originalString.ToLower();
+Console.WriteLine(LowerCaseString);
+
+//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string str = "МАШИНА";
+bool isPolindrome = true;
+for (int i = 0; i < str.Length / 2; i++)
+{
+    if (str[i] != str[str.Length - 1 - i])
+    {
+        isPolindrome = false;
+        break;
+    }
+}
+if (isPolindrome)
+{
+    Console.WriteLine($"{str} является полиндромом");
+}
+   else
+{
+ Console.WriteLine($"{str} не является полиндромом");   
+}
+
+
+  
